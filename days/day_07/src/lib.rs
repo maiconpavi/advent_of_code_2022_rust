ad5a1faba1d3ae2cbda77467de426989633ce795
maiconpavi <75434446+maiconpavi@users.noreply.github.com>
2023-07-26 19:38:27 -0300

    refactoring: day_07

--- days/day_07/src/lib.rs
+++ days/day_07/src/lib.rs
@@ -17,9 +17,9 @@ pub fn calc_a(input: &str) -> String {
 pub fn calc_b(input: &str) -> String {
     let goal = 40_000_000_usize;
     let (_, folders) = get_files(input);
-    let used_space = folders.get(&PathBuf::from("/")).unwrap();
+    let used_space = folders.get(&PathBuf::from("/")).expect("No root folder");
     if *used_space < goal {
-        return "0".to_string();
+        "0".to_string()
     } else {
         let to_remove = used_space - goal;
         let mut folders = folders
@@ -28,10 +28,11 @@ pub fn calc_b(input: &str) -> String {
             .map(|(_, size)| size)
             .collect::<Vec<_>>();
         folders.sort_unstable();
-        folders.first().unwrap().to_string()
+        folders.first().expect("Empty folders").to_string()
     }
 }
 
+#[allow(clippy::option_if_let_else)]
 fn get_files(input: &str) -> (Vec<(PathBuf, usize)>, HashMap<PathBuf, usize>) {
     let mut files = Vec::new();
     let commands = parse_commands(input).skip(1).collect::<Vec<_>>();
@@ -47,11 +48,8 @@ fn get_files(input: &str) -> (Vec<(PathBuf, usize)>, HashMap<PathBuf, usize>) {
             }
             Command::Ls { content } => {
                 for entry in content.iter() {
-                    match entry {
-                        RawDirEntry::File { name, size } => {
-                            files.push((current_dir.join(name), *size));
-                        }
-                        _ => {}
+                    if let RawDirEntry::File { name, size } = entry {
+                        files.push((current_dir.join(name), *size));
                     }
                 }
             }
@@ -59,7 +57,7 @@ fn get_files(input: &str) -> (Vec<(PathBuf, usize)>, HashMap<PathBuf, usize>) {
     }
     let mut folders = HashMap::<PathBuf, usize>::new();
     for (path, size) in &files {
-        let mut path = path.to_path_buf();
+        let mut path = path.clone();
         while let Some(parent) = path.parent() {
             path = parent.to_path_buf();
             if let Some(f) = folders.get_mut(&path) {
@@ -73,11 +71,11 @@ fn get_files(input: &str) -> (Vec<(PathBuf, usize)>, HashMap<PathBuf, usize>) {
     (files, folders)
 }
 
-fn parse_commands<'a>(input: &'a str) -> impl Iterator<Item = Command<'a>> + 'a {
+fn parse_commands(input: &str) -> impl Iterator<Item = Command<'_>> + '_ {
     input
         .trim_matches(['\n', '$', ' '].as_slice())
         .split("\n$ ")
-        .map(Command::from)
+        .filter_map(|line| Command::try_from(line).ok())
 }
 
 enum Command<'a> {
@@ -90,29 +88,29 @@ enum RawDirEntry<'a> {
     Dir { name: &'a str },
 }
 
-impl<'a> From<&'a str> for Command<'a> {
-    fn from(s: &'a str) -> Self {
+impl<'a> TryFrom<&'a str> for Command<'a> {
+    type Error = String;
+    fn try_from(s: &'a str) -> Result<Self, Self::Error> {
         match &s[..2] {
-            "cd" => Command::Cd { path: &s[3..] },
-            "ls" => Command::Ls {
+            "cd" => Ok(Command::Cd { path: &s[3..] }),
+            "ls" => Ok(Command::Ls {
                 content: s[3..].split('\n').map(RawDirEntry::from).collect(),
-            },
-            cmd => panic!("Invalid command: {}", cmd),
+            }),
+            cmd => Err(format!("Invalid command: {cmd}")),
         }
     }
 }
 
 impl<'a> From<&'a str> for RawDirEntry<'a> {
     fn from(s: &'a str) -> Self {
-        match &s[..3] {
-            "dir" => RawDirEntry::Dir { name: &s[4..] },
-            _ => {
-                let (raw_size, name) = s.split_once(" ").expect("Invalid file entry");
+        if &s[..3] == "dir" {
+            RawDirEntry::Dir { name: &s[4..] }
+        } else {
+            let (raw_size, name) = s.split_once(' ').expect("Invalid file entry");
 
-                RawDirEntry::File {
-                    name,
-                    size: raw_size.parse().expect("Invalid file size"),
-                }
+            RawDirEntry::File {
+                name,
+                size: raw_size.parse().expect("Invalid file size"),
             }
         }
     }
